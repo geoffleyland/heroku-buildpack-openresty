215a4ec66bcec47cc88164ae9ac0231602a511d1
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-02-26 08:53:54 +1300

    try some exports on those environment variables

--- .profile.d/openresty.sh
+++ .profile.d/openresty.sh
@@ -1,4 +1,4 @@
 PATH=/app/tools/bin:$PATH
-LD_LIBRARY_PATH=/app/tools/lib
-LUA_PATH=/app/tools/share/lua/5.1/?.lua
-LUA_CPATH=/app/tools/lib/lua/5.1/?.so
+export LD_LIBRARY_PATH=/app/tools/lib
+export LUA_PATH=/app/tools/share/lua/5.1/?.lua
+export LUA_CPATH=/app/tools/lib/lua/5.1/?.so
