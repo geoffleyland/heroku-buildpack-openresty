7a7372ce5643a44aeded2537b3b7e1a138ae2df0
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-02-26 13:52:49 +1300

    don't write the number of replacements to the template

--- tools/template.lua
+++ tools/template.lua
@@ -1,2 +1,2 @@
 #!/usr/bin/env lua
-io.write(io.read("*a"):gsub("%$%(([%w_]+)%)", function(w) return os.getenv(w) or w end))
+io.write((io.read("*a"):gsub("%$%(([%w_]+)%)", function(w) return os.getenv(w) or w end)))
