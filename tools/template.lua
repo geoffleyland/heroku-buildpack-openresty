55eda158310bf802276930ef05b3f73dd4a3db49
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-02-27 15:11:56 +1300

    Change the template syntax to ${} and add default value functionality like the shell: ${PORT:-8080}

--- tools/template.lua
+++ tools/template.lua
@@ -1,14 +1,21 @@
 #!/usr/bin/env lua
 -- nginx doesn't use variables in its config file, so we have to fake them -
 -- for example the port we should listen to comes through as PORT.
--- So here, we take a file and replace all the instances of "%(VARIABLE)"
--- with os.getenv(VARIABLE)
+-- So here, we take a file and replace all the instances of "${VARIABLE}"
+-- with os.getenv(VARIABLE).
+-- We also understand ${VARIABLE:-default} syntax, which proves useful if
+-- you're trying to run locally as well as on Heroku.  For example, if
+-- PORT is not defined locally, you can (in nginx.conf) go
+-- listen ${PORT:-8080};
+-- and it'll connect to 8080 when running locally.
 
-io.write((
-  io.read("*a"):gsub("%$%(([%w_]+)%)",
-                     function(w) return os.getenv(w) or w end)
-  ))
+io.write((io.read("*a"):gsub("%$(%b{})", function(w)
+      w = w:sub(2,-2)
+      local variable, default = w:match("(.+):[-=](.+)")
+      return os.getenv(variable or w) or default or w
+    end
+  )))
 
--- io.write is in braces because gsub returns two values - the modified
+-- io.write has double braces because gsub returns two values - the modified
 -- string and the number of substitions it made.  If you're not careful
 -- we'd write the number of substitutions to the end of the config file.
