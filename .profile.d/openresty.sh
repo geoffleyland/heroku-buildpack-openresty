117e2e339a7b5e2c57d70ca93370b5e3c986034f
Geoff Leyland <geoff.leyland@incremental.co.nz>
2015-03-11 17:05:09 +1300

    add ./?.lua to LUA_PATH

--- .profile.d/openresty.sh
+++ .profile.d/openresty.sh
@@ -2,5 +2,5 @@ TOOLS=$HOME/tools
 
 PATH=$TOOLS/bin:$PATH
 export LD_LIBRARY_PATH=$TOOLS/lib
-export LUA_PATH=$TOOLS/share/lua/5.1/?.lua
+export LUA_PATH=./?.lua;$TOOLS/share/lua/5.1/?.lua
 export LUA_CPATH=$TOOLS/lib/lua/5.1/?.so
